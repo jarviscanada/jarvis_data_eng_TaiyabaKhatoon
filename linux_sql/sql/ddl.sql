3f48260112b5c00ac93a446160b5dacc95e26263
Taiyaba Khatoon <rocky@jrvs-remote-desktop.northamerica-northeast2-b.c.project-3a98ff34-1e0c-4f04-ae2.internal>
2026-02-04 05:28:00 +0000

    Resolve merge conflict in ddl.sql and switch to host_agent database

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,4 +1,6 @@
-<<<<<<< HEAD
+-- switch to host_agent database
+\c host_agent;
+
 -- Create a host_info table
 CREATE TABLE IF NOT EXISTS PUBLIC.host_info
 (
@@ -26,33 +28,5 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
   disk_io INT4 NOT NULL,
   disk_available INT4 NOT NULL,
   CONSTRAINT host_usage_host_info_fk
-    FOREIGN KEY (host_id) REFERENCES host_info(id)
-=======
-\c host_agent;
-
-DROP TABLE IF EXISTS host_usage;
-DROP TABLE IF EXISTS host_info;
-
-CREATE TABLE host_info (
-  id SERIAL PRIMARY KEY,
-  hostname VARCHAR(255) UNIQUE NOT NULL,
-  cpu_number INT NOT NULL,
-  cpu_architecture VARCHAR(50) NOT NULL,
-  cpu_model VARCHAR(255) NOT NULL,
-  cpu_mhz FLOAT NOT NULL,
-  l2_cache INT NOT NULL,
-  total_mem INT NOT NULL,
-  timestamp TIMESTAMP NOT NULL
-);
-
-CREATE TABLE host_usage (
-  timestamp TIMESTAMP NOT NULL,
-  host_id INT NOT NULL,
-  memory_free INT NOT NULL,
-  cpu_idle INT NOT NULL,
-  cpu_kernel INT NOT NULL,
-  disk_io INT NOT NULL,
-  disk_available INT NOT NULL,
-  CONSTRAINT fk_host_usage_host_info FOREIGN KEY (host_id) REFERENCES host_info(id)
->>>>>>> feature/psql_docker
+    FOREIGN KEY (host_id) REFERENCES public.host_info(id)
 );
